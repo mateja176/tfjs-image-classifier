e9f4d3e41adb50859807c836de8df4b069c1c5b3
Mateja Petrovic <mateja176@gmail.com>
2019-06-19 23:07:33 +0200

    feat: adds webcam image classification app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
 
 const img = document.querySelector('img');
+const webcam = document.getElementById('webcam') as HTMLVideoElement;
 
-(async function app() {
+const imageClassificationApp = async () => {
   const net = await mobilenet.load();
 
   const result = await net.classify(img);
 
   console.log(result);
-})();
+};
+
+const setupWebcam = async () => {
+  return new Promise((resolve, reject) => {
+    navigator.getUserMedia(
+      { video: true },
+      stream => {
+        webcam.srcObject = stream;
+        webcam.addEventListener('loadeddata', () => resolve(), false);
+      },
+      error => reject(error.message),
+    );
+  });
+};
+
+const webcamImageClassificationApp = async () => {
+  const net = await mobilenet.load();
+
+  await setupWebcam();
+
+  const classify = () =>
+    setTimeout(async () => {
+      const [result] = await net.classify(webcam);
+      console.table(result);
+
+      await tf.nextFrame();
+      classify();
+    }, 2000);
+
+  classify();
+};
+
+webcamImageClassificationApp();
